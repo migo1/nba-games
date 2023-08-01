97984c9843d4e625039cfae839c610487973e732
migo1 <ertmigot@gmail.com>
2023-08-01 08:10:03 +0300

    add-style

--- src/components/teamStats/TeamStats.js
+++ src/components/teamStats/TeamStats.js
@@ -38,14 +38,111 @@ const TeamStats = () => {
             </div>
           </div>
           <ul>
-            <h3 className="pl-12 text-white mb-2 text-lg">GAMES</h3>
+            <h3 className="pl-12 text-white mb-2 text-lg">GAMES PLAYED</h3>
             <li className="list-stats">
-              <p>Played Home</p>
+              <p> Home</p>
               <p>{teamStats.teamStats.games.played.home}</p>
             </li>
             <li className="list-stats">
-              <p>Played Away</p>
+              <p> Away</p>
               <p>{teamStats.teamStats.games.played.away}</p>
+            </li>
+            <li className="list-stats">
+              <p> ALL</p>
+              <p>{teamStats.teamStats.games.played.all}</p>
+            </li>
+            <h3 className="pl-12 text-white mb-2 mt-2 text-lg">GAMES WINS</h3>
+            <li className="list-stats">
+              <p> Home</p>
+              <p>{teamStats.teamStats.games.wins.home.total}</p>
+            </li>
+            <li className="list-stats">
+              <p> Home percentage (%)</p>
+              <p>{teamStats.teamStats.games.wins.home.percentage}</p>
+            </li>
+            <li className="list-stats">
+              <p> Away</p>
+              <p>{teamStats.teamStats.games.wins.away.total}</p>
+            </li>
+            <li className="list-stats">
+              <p> Away percentage (%)</p>
+              <p>{teamStats.teamStats.games.wins.away.percentage}</p>
+            </li>
+            <li className="list-stats">
+              <p> All</p>
+              <p>{teamStats.teamStats.games.wins.all.total}</p>
+            </li>
+            <li className="list-stats">
+              <p> All percentage (%)</p>
+              <p>{teamStats.teamStats.games.wins.all.percentage}</p>
+            </li>
+            <h3 className="pl-12 text-white mb-2 mt-2 text-lg">GAMES LOSES</h3>
+            <li className="list-stats">
+              <p> Home</p>
+              <p>{teamStats.teamStats.games.loses.home.total}</p>
+            </li>
+            <li className="list-stats">
+              <p> Home percentage (%)</p>
+              <p>{teamStats.teamStats.games.loses.home.percentage}</p>
+            </li>
+            <li className="list-stats">
+              <p> Away</p>
+              <p>{teamStats.teamStats.games.loses.away.total}</p>
+            </li>
+            <li className="list-stats">
+              <p> Away percentage (%)</p>
+              <p>{teamStats.teamStats.games.loses.away.percentage}</p>
+            </li>
+            <li className="list-stats">
+              <p> All</p>
+              <p>{teamStats.teamStats.games.loses.all.total}</p>
+            </li>
+            <li className="list-stats">
+              <p> All percentage (%)</p>
+              <p>{teamStats.teamStats.games.loses.all.percentage}</p>
+            </li>
+            <h3 className="pl-12 text-white mb-2 text-lg">POINTS FOR TOTAL</h3>
+            <li className="list-stats">
+              <p> Home</p>
+              <p>{teamStats.teamStats.points.for.total.home}</p>
+            </li>
+            <li className="list-stats">
+              <p> Away</p>
+              <p>{teamStats.teamStats.points.for.total.away}</p>
+            </li>
+            <li className="list-stats">
+              <p> All</p>
+              <p>{teamStats.teamStats.points.for.total.all}</p>
+            </li>
+            <h3 className="pl-12 text-white mb-2 text-lg">
+              POINTS FOR AVERAGE
+            </h3>
+            <li className="list-stats">
+              <p> Home</p>
+              <p>{teamStats.teamStats.points.for.average.home}</p>
+            </li>
+            <li className="list-stats">
+              <p> Away</p>
+              <p>{teamStats.teamStats.points.for.average.away}</p>
+            </li>
+            <li className="list-stats">
+              <p> All</p>
+              <p>{teamStats.teamStats.points.for.average.all}</p>
+            </li>
+            <h3 className="pl-12 text-white mb-2 text-lg">
+              POINTS AGAINST AVERAGE
+            </h3>
+            <li className="list-stats">
+              <p> Home</p>
+              <p>{teamStats.teamStats.points.against.average.home}</p>
+            </li>
+            <li className="list-stats">
+              <p> Away</p>
+              <p>{teamStats.teamStats.points.against.average.away}</p>
+            </li>
+            <li className="list-stats">
+              <p> All</p>
+              <p>{teamStats.teamStats.points.against.average.all}</p>
             </li>
           </ul>
         </div>
