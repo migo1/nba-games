ee403cd643604e8fac7ecefa3dbbc45b6d39700c
migo1 <ertmigot@gmail.com>
2023-08-02 15:39:23 +0300

    header-responsive

--- src/components/teams/Header.js
+++ src/components/teams/Header.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './header.css';
 
 function Header({ searchQuery, handleSearchInputChange }) {
   return (
-    <div className="flex justify-between w-12/12 mx-auto bg-sky-950 py-3 px-12 items-center">
-      <p className="text-white">NBA Teams Standings for Season 2019 - 2020</p>
+    <div className="flex justify-between w-12/12 mx-auto bg-sky-950 py-3 px-12 items-center ">
+      <p className="text-white toogle-hide">
+        NBA Teams Standings for Season 2019 - 2020
+      </p>
       <input
         type="text"
         placeholder="Search teams by name"
         value={searchQuery}
         onChange={handleSearchInputChange}
-        className="w-4/12 px-3 py-2 rounded-md"
+        className="w-4/12 px-3 py-2 rounded-md search"
       />
     </div>
   );
